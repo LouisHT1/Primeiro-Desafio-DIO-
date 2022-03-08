3fb4f2c367aed588142f666d49d48053fac9d289
Louis <luizhtred@outlook.com>
2022-03-08 18:01:09 -0300

    Modificação Html/adição da pasta javascript

---
+++ JavaScript ES6 essencial/teste.js
@@ -0,0 +1,8 @@
+// Retorna um array quebrando a string por um delimitador
+
+const textoSep = 'Texto'.split('x')
+console.log('\nArray com as posições separadas pelo delimitador')
+
+// Busca por um valor e substitui por outro 
+
+const replacedText = 'Texto'.replace('Text', 'txeT')
